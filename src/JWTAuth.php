c8e7d0b65a5dda7b9ff5107595729f69be7083c8
liusongsen <liusongsen@gmail.com>
2018-11-04 11:21:49 +0800

    合并冲突

--- src/JWTAuth.php
+++ src/JWTAuth.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of jwt-auth.
  *
@@ -8,12 +7,9 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Tymon\JWTAuth;
-
 use Tymon\JWTAuth\Http\Parser\Parser;
 use Tymon\JWTAuth\Contracts\Providers\Auth;
-
 class JWTAuth extends JWT
 {
     /**
@@ -22,7 +18,6 @@ class JWTAuth extends JWT
      * @var \Tymon\JWTAuth\Contracts\Providers\Auth
      */
     protected $auth;
-
     /**
      * Constructor.
      *
@@ -37,7 +32,6 @@ class JWTAuth extends JWT
         parent::__construct($manager, $parser);
         $this->auth = $auth;
     }
-
     /**
      * Attempt to authenticate the user and return the token.
      *
@@ -50,129 +44,21 @@ class JWTAuth extends JWT
         if (! $this->auth->byCredentials($credentials)) {
             return false;
         }
-
         return $this->fromUser($this->user());
     }
-
     /**
      * Authenticate a user via a token.
      *
-<<<<<<< HEAD
      * @return \Tymon\JWTAuth\Contracts\JWTSubject|false
-=======
-     * @param mixed $token
-     *
-     * @return mixed
-     */
-    public function authenticate($token = false)
-    {
-        $id = $this->getPayload($token)->get('sub');
-
-        if (! $this->auth->byId($id)) {
-            return false;
-        }
-
-        return $this->auth->user();
-    }
-
-    /**
-     * Refresh an expired token.
-     *
-     * @param mixed $token
-     *
-     * @return string
-     */
-    public function refresh($token = false)
-    {
-        $this->requireToken($token);
-
-        return $this->manager->refresh($this->token)->get();
-    }
-
-    /**
-     * Invalidate a token (add it to the blacklist).
-     *
-     * @param mixed $token
-     *
-     * @return bool
-     */
-    public function invalidate($token = false)
-    {
-        $this->requireToken($token);
-
-        return $this->manager->invalidate($this->token);
-    }
-
-    /**
-     * Get the token.
-     *
-     * @return bool|string
-     */
-    public function getToken()
-    {
-        if (! $this->token) {
-            try {
-                $this->parseToken();
-            } catch (JWTException $e) {
-                return false;
-            }
-        }
-
-        return $this->token;
-    }
-
-    /**
-     * Get the raw Payload instance.
-     *
-     * @param mixed $token
-     *
-     * @return \Tymon\JWTAuth\Payload
-     */
-    public function getPayload($token = false)
-    {
-        $this->requireToken($token);
-
-        return $this->manager->decode($this->token);
-    }
-
-    /**
-     * Parse the token from the request.
-     *
-     * @param string $query
-     *
-     * @return JWTAuth
-     */
-    public function parseToken($method = 'bearer', $header = 'authorization', $query = 'token')
-    {
-        if (! $token = $this->parseAuthHeader($header, $method)) {
-            if (! $token = $this->request->query($query, false)) {
-                throw new JWTException('The token could not be parsed from the request', 400);
-            }
-        }
-
-        return $this->setToken($token);
-    }
-
-    /**
-     * Parse token from the authorization header.
-     *
-     * @param string $header
-     * @param string $method
-     *
-     * @return false|string
->>>>>>> 614ee3410a1cc18ef13c8d5ffd491b5608afabd8
      */
     public function authenticate()
     {
-        $staff_id = $this->getPayload()->get('user_id');
-
-        if (! $this->auth->byId($staff_id)) {
+        $id = $this->getPayload()->get('user_id');
+        if (! $this->auth->byId($id)) {
             return false;
         }
-
         return $this->user();
     }
-
     /**
      * Alias for authenticate().
      *
@@ -182,7 +68,6 @@ class JWTAuth extends JWT
     {
         return $this->authenticate();
     }
-
     /**
      * Get the authenticated user.
      *
@@ -190,73 +75,6 @@ class JWTAuth extends JWT
      */
     public function user()
     {
-<<<<<<< HEAD
         return $this->auth->user();
-=======
-        $this->token = new Token($token);
-
-        return $this;
-    }
-
-    /**
-     * Ensure that a token is available.
-     *
-     * @param mixed $token
-     *
-     * @return JWTAuth
-     *
-     * @throws \Tymon\JWTAuth\Exceptions\JWTException
-     */
-    protected function requireToken($token)
-    {
-        if ($token) {
-            return $this->setToken($token);
-        } elseif ($this->token) {
-            return $this;
-        } else {
-            throw new JWTException('A token is required', 400);
-        }
-    }
-
-    /**
-     * Set the request instance.
-     *
-     * @param Request $request
-     */
-    public function setRequest(Request $request)
-    {
-        $this->request = $request;
-
-        return $this;
-    }
-
-    /**
-     * Get the JWTManager instance.
-     *
-     * @return \Tymon\JWTAuth\JWTManager
-     */
-    public function manager()
-    {
-        return $this->manager;
-    }
-
-    /**
-     * Magically call the JWT Manager.
-     *
-     * @param string $method
-     * @param array  $parameters
-     *
-     * @return mixed
-     *
-     * @throws \BadMethodCallException
-     */
-    public function __call($method, $parameters)
-    {
-        if (method_exists($this->manager, $method)) {
-            return call_user_func_array([$this->manager, $method], $parameters);
-        }
-
-        throw new \BadMethodCallException("Method [$method] does not exist.");
->>>>>>> 614ee3410a1cc18ef13c8d5ffd491b5608afabd8
     }
 }
